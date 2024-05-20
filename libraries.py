a8b3053a1ba8a94c1abf9392bf578c5982e1f129
zuhaaaaaaa <169088195+zuhaaaaaaa@users.noreply.github.com>
2024-05-20 01:17:48 -0400

    Create libraries.py
    It is necessary libraries

---
+++ libraries.py
@@ -0,0 +1,20 @@
+!nvidia-smi
+!pip install torch torchvision
+!pip install matplotlib-venn
+!pip install accelerate
+!apt-get -qq install -y libfluidsynth1
+!pip install transformers
+!pip install opencv-python
+!pip install torch
+!pip install torch diffusers transformers
+!pip install torch diffusers transformers gradio
+!pip install --upgrade gradio
+!pip show gradio
+!pip install gradio
+!pip install torch torchvision
+!pip show diffusers
+!pip install diffusers
+!pip install --upgrade diffusers
+!pip install --upgrade transformers
+!pip show segmentation-models-pytorch
+!pip install --upgrade segmentation-models-pytorch
